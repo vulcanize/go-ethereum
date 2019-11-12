fd503cf63d5381f0d93b529bd5e631bcb18fec97
Ian Norden <iannordenn@gmail.com>
2019-11-12 14:19:15 -0600

    apply gas pool/trx execution changes across pkgs

--- tests/state_test_util.go
+++ tests/state_test_util.go
@@ -199,8 +199,12 @@ func (t *StateTest) RunNoVerify(subtest StateSubtest, vmconfig vm.Config, snapsh
 	}
 	gaspool := new(core.GasPool)
 	gaspool.AddGas(block.GasLimit())
+	var gp1559 *core.GasPool
+	if config.IsEIP1559(block.Number()) {
+		gp1559 = new(core.GasPool).AddGas(params.MaxGasEIP1559)
+	}
 	snapshot := statedb.Snapshot()
-	if _, err := core.ApplyMessage(evm, msg, gaspool); err != nil {
+	if _, err := core.ApplyMessage(evm, msg, gaspool, gp1559); err != nil {
 		statedb.RevertToSnapshot(snapshot)
 	}
 	// Commit block
